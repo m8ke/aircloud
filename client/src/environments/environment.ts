a3d887767de3bb4a2f29100f316a0b1a29a979ca
Mike <91439210+m8ke@users.noreply.github.com>
2025-08-28 15:29:16 +0300

    build: add Docker, Nginx and deploy pipeline (#21)

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     version: "1",
     production: true,
-    wsUrl: "wss://localhost/ws", // TODO: Add later
-    clientUrl: "https://localhost", // TODO: Add later
+    wsUrl: "wss://aircloud.sh/ws",
+    clientUrl: "https://aircloud.sh",
 };
