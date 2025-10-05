720551c17b784a77dfbe0ff71ba2a350728e25bb
m8ke <91439210+m8ke@users.noreply.github.com>
2025-10-05 04:24:00 -0700

    feat: add native share button

--- client/src/app/ui/layout/layout.ts
+++ client/src/app/ui/layout/layout.ts
@@ -5,6 +5,7 @@ import { CdkDropList } from "@angular/cdk/drag-drop";
 import { FileManager } from "@/utils/file-manager/file-manager";
 import { Notification } from "@/ui/notification/notification";
 import { NotificationService, NotificationType } from "@/ui/notification/notification.service";
+import { Env } from "@/utils/env/env";
 
 @Component({
     selector: "app-layout",
@@ -17,8 +18,8 @@ import { NotificationService, NotificationType } from "@/ui/notification/notific
     styleUrl: "./layout.scss",
 })
 export class Layout {
+    private readonly env: Env = inject<Env>(Env);
     private readonly fileManager: FileManager = inject(FileManager);
-    private readonly notification: NotificationService = inject(NotificationService);
     protected readonly isFileDropping = signal<boolean>(false);
 
     protected onDragOver(event: DragEvent): void {
@@ -42,5 +43,25 @@ export class Layout {
         event.preventDefault();
         event.stopPropagation();
         this.isFileDropping.set(false);
+    }
+
+    protected async nativeShare(): Promise<void> {
+        if (navigator.share && window) {
+            try {
+                await navigator.share({
+                    title: "AirCloud",
+                    text: "Share files securely and free!",
+                    url: this.env.clientUrl,
+                });
+            } catch (error) {
+                console.warn(error);
+            }
+        } else {
+            console.warn("Sharing not supported on this browser.");
+        }
+    }
+
+    protected get isNativeShareEnabled(): boolean {
+        return !!navigator.share;
     }
 }
